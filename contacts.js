911c176205702561be74178dc661256ecd24a8f4
Yevgen Savchuk <107425775+JS1170@users.noreply.github.com>
2022-11-13 17:49:11 +0200

    files update

---
+++ contacts.js
@@ -0,0 +1,55 @@
+const fs = require("fs/promises");
+const path = require("path");
+const ObjectID = require("bson-objectid");
+
+const contactsPath = path.join(__dirname, "db/contacts.json");
+
+const updateContacts = async (contacts) => {
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+};
+
+const listContacts = async () => {
+  const result = await fs.readFile(contactsPath);
+  return JSON.parse(result);
+};
+
+const getById = async (id) => {
+    const contacts = await listContacts();
+    const result = await contacts.find(contact => contact.id === id);
+    if (!result) {
+      return null;
+    }
+    return result;
+};
+
+const addContact = async (name, email, phone) => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: ObjectID(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  updateContacts(contacts);
+  return newContact;
+};
+
+const removeById = async (id) => {
+  const contacts = await listContacts();
+  const idx = contacts.findIndex(item => item.id === id);
+  if (idx === -1) {
+    return null;
+  }
+
+  const [result] = contacts.splice(idx, 1);
+  updateContacts(contacts);
+  return result;
+};
+
+module.exports = {
+  listContacts,
+  getById,
+  addContact,
+  removeById,
+};
---
+++ index.js
@@ -0,0 +1,35 @@
+const contacts = require("./contacts");
+const argv = require("yargs").argv;
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      console.log(allContacts);
+      break;
+
+    case "get":
+      const getContactById = await contacts.getById(id);
+      console.log(getContactById);
+      break;
+
+    case "add":
+      const newContact = await contacts.addContact(name, email, phone);
+      console.log(newContact);
+      break;
+
+    case "remove":
+      const removeContact = await contacts.removeById(id);
+      console.log(removeContact);
+      break;
+
+    default:
+      console.warn("Unknown action");
+  }
+};
+
+invokeAction(argv);
+
+
+
+
